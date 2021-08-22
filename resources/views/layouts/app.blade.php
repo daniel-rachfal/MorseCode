b713203ab85072ad8d362cace532ab2b7c9d48ac
Daniel Rachfal <rachfaldaniel@gmail.com>
2021-08-22 12:21:16 +0000

    Added navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,24 @@
 
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>Morse Trainer - @yield('title')</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     </head>
     <body>
-        @section('sidebar')
-        @show
+        <div class="container">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/morseToText">Morse To Text</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/textToMorse">Text To Morse</a>
+                </li>
+            </ul>
+        </div>
 
         <div class="container">
             @yield('content')
---
+++ resources/views/morseToText.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('title', 'Morse To Text')
+@section('content')
+<h2>{{$result ?? ''}}</h2>
+
+<br>
+<h2>Translate the given audio to text!<h2>
+<audio controls="controls" height="100" width="1000" class="form-control">
+<source src="{{url('storage/'.$morseCode->fileLocation)}}" type="audio/ogg" />
+<embed height="100" width="100" src="{{url('storage/'.$morseCode->fileLocation)}}" />
+</audio>
+
+<form action="{{url('/morseToText')}}" method="POST" >
+    {{ csrf_field() }}
+    <input type="hidden" name="correct" value="{{$morseCode->plainText}}">
+    <input type="text" name="answer" placeholder="Text" class="form-control">
+    <input type="submit" value="Submit" class="form-control">
+</form>
+
+@endsection
